8f611161976dd289debf8369e36da267e697f4db
Philippe Steinmann <philippe.steinmann@gmail.com>
2020-02-29 09:54:21 +0100

    First skeleton of source

--- src/Mdt/UicNumber/Algorithm.h
+++ src/Mdt/UicNumber/Algorithm.h
@@ -4,17 +4,12 @@
  * (See accompanying file LICENSE.txt or copy at
  * https://www.boost.org/LICENSE_1_0.txt)
  */
-#ifndef MDT_UIC_NUMBER_UIC_NUMBER_H
-#define MDT_UIC_NUMBER_UIC_NUMBER_H
+#ifndef MDT_UIC_NUMBER_ALGORITHM_H
+#define MDT_UIC_NUMBER_ALGORITHM_H
 
 namespace Mdt{ namespace UicNumber{
 
-  /*! \brief
-   */
-  class UicNumber
-  {
-  };
-
+  
 }} // namespace Mdt{ namespace UicNumber{
 
-#endif // #ifndef MDT_UIC_NUMBER_UIC_NUMBER_H
+#endif // #ifndef MDT_UIC_NUMBER_ALGORITHM_H
---
+++ tests/src/UicNumberTest.cpp
@@ -0,0 +1,6 @@
+#include "catch2/catch.hpp"
+#include "Mdt/UicNumber/UicNumber"
+
+TEST_CASE("Nothing")
+{
+}
