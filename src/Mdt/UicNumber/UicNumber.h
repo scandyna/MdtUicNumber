ec27ce2b662b92361ae6a46b5016cdd74b04a750
Philippe Steinmann <philippe.steinmann@gmail.com>
2020-02-29 10:27:00 +0100

    Work on UicNumber

--- src/Mdt/UicNumber/UicNumber.h
+++ src/Mdt/UicNumber/UicNumber.h
@@ -9,10 +9,42 @@
 
 namespace Mdt{ namespace UicNumber{
 
-  /*! \brief
+  /*! \brief Represents a 12 digit UIC number
+   *
+   * A UIC number 3 main blocks:
+   * - The international block, digits 1-4
+   * - The national block, digits 5-11
+   * - A check digit, digit 12
+   *
+   * The international block is composed of a type code and a country code.
+   * The UIC number can finaly be representad by 4 blocks:
+   * - The type code, digits 1-2
+   * - The country code, digits 3-4
+   * - The national block, digits 5-11
+   * - A check digit, digit 12
+   *
+   * \sa https://en.wikipedia.org/wiki/UIC_identification_marking_for_tractive_stock
+   * \todo See if the class has to be a template, think not (can have function templates for StringType)
    */
   class UicNumber
   {
+   public:
+
+    /*! \brief Validate a UIC number string
+     *
+     * \tparam StringType A STL compatible string ......
+     * \todo Comparator ?
+     * \todo Mybe in algorithm..
+     */
+    template<typename StringType>
+    static
+    bool validate(const StringType & uicNumberString)
+    {
+    }
+
+   private:
+
+    
   };
 
 }} // namespace Mdt{ namespace UicNumber{
