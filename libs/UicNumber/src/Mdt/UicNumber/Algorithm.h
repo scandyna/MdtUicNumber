8c8995b711e90067696051ef5139cea79209b08c
Philippe Steinmann <philippe.steinmann@gmail.com>
2020-03-16 13:55:15 +0100

    Implement xDigitFromValueBase10() functions

--- libs/UicNumber/src/Mdt/UicNumber/Algorithm.h
+++ libs/UicNumber/src/Mdt/UicNumber/Algorithm.h
@@ -56,6 +56,19 @@ namespace Mdt{ namespace UicNumber{
   }
 
   /*! \internal
+   *
+   * \pre \a i must represent a single digit [0-9]
+   */
+  inline
+  char charFromInt8(int8_t i)
+  {
+    assert( i >= 0 );
+    assert( i <= 9 );
+
+    return i + '0';
+  }
+
+  /*! \internal
    */
   inline
   int toInt(const std::string & str, int position, int length)
@@ -65,7 +78,120 @@ namespace Mdt{ namespace UicNumber{
     return std::stoi(std::string(str, position, length), nullptr, 10);
   }
 
+  /*! \brief Get the ones digit from \a value
+   *
+   * Examples:
+   * \code
+   * onceDigitFromValueBase10(1) == 1
+   * onceDigitFromValueBase10(12) == 2
+   * onceDigitFromValueBase10(123) == 3
+   * \endcode
+   */
+  inline
+  int onceDigitFromValueBase10(int value)
+  {
+    return value % 10;
+  }
 
+  /*! \brief Get the tens digit from \a value
+   *
+   * Examples:
+   * \code
+   * tensDigitFromValueBase10(1) == 0
+   * tensDigitFromValueBase10(12) == 1
+   * tensDigitFromValueBase10(123) == 2
+   * \endcode
+   */
+  inline
+  int tensDigitFromValueBase10(int value)
+  {
+    const int tens = value % 100;
+    if(tens < 10){
+      return 0;
+    }
+    return (tens - onceDigitFromValueBase10(value)) / 10;
+  }
+
+  /*! \brief Get the hundreds digit from \a value
+   *
+   * Examples:
+   * \code
+   * hundredsDigitFromValueBase10(1) == 0
+   * hundredsDigitFromValueBase10(12) == 0
+   * hundredsDigitFromValueBase10(123) == 1
+   * hundredsDigitFromValueBase10(1234) == 2
+   * \endcode
+   */
+  inline
+  int hundredsDigitFromValueBase10(int value)
+  {
+    const int hundreds = value % 1000;
+    if(hundreds < 100){
+      return 0;
+    }
+    return (hundreds - tensDigitFromValueBase10(value)) / 100;
+  }
+
+  /*! \brief Get the thousands digit from \a value
+   *
+   * Examples:
+   * \code
+   * thousandsDigitFromValueBase10(1) == 0
+   * thousandsDigitFromValueBase10(12) == 0
+   * thousandsDigitFromValueBase10(123) == 0
+   * thousandsDigitFromValueBase10(1234) == 1
+   * thousandsDigitFromValueBase10(12345) == 2
+   * \endcode
+   */
+  inline
+  int thousandsDigitFromValueBase10(int value)
+  {
+    const int thousands = value % 10000;
+    if(thousands < 1000){
+      return 0;
+    }
+    return (thousands - hundredsDigitFromValueBase10(value)) / 1000;
+  }
+
+  /*! \brief Get the ten-thousands digit from \a value
+   *
+   * Examples:
+   * \code
+   * tenThousandsDigitFromValueBase10(123) == 0
+   * tenThousandsDigitFromValueBase10(1234) == 0
+   * tenThousandsDigitFromValueBase10(12345) == 1
+   * tenThousandsDigitFromValueBase10(123456) == 2
+   * \endcode
+   */
+  inline
+  int tenThousandsDigitFromValueBase10(int value)
+  {
+    const int tenThousands = value % 100000;
+    if(tenThousands < 10000){
+      return 0;
+    }
+    return (tenThousands - thousandsDigitFromValueBase10(value)) / 10000;
+  }
+
+  /*! \brief Get the hundred-thousands digit from \a value
+   *
+   * Examples:
+   * \code
+   * hundredThousandsDigitFromValueBase10(1'234) == 0
+   * hundredThousandsDigitFromValueBase10(12'345) == 0
+   * hundredThousandsDigitFromValueBase10(123'456) == 1
+   * hundredThousandsDigitFromValueBase10(1'234'567) == 2
+   * \endcode
+   */
+  inline
+  int hundredThousandsDigitFromValueBase10(int value)
+  {
+    const int hundredThousands = value % 1000000;
+    if(hundredThousands < 100000){
+      return 0;
+    }
+    return (hundredThousands - tenThousandsDigitFromValueBase10(value)) / 100000;
+  }
 
   /*! \brief Check if \a c is allowed in a UIC number string
    *
--- libs/UicNumber/tests/src/AlgorithmTest.cpp
+++ libs/UicNumber/tests/src/AlgorithmTest.cpp
@@ -4,7 +4,14 @@
 
 using Mdt::UicNumber::UicNumber11DigitArray;
 using Mdt::UicNumber::int8FromChar;
+using Mdt::UicNumber::charFromInt8;
 using Mdt::UicNumber::toInt;
+using Mdt::UicNumber::onceDigitFromValueBase10;
+using Mdt::UicNumber::tensDigitFromValueBase10;
+using Mdt::UicNumber::hundredsDigitFromValueBase10;
+using Mdt::UicNumber::thousandsDigitFromValueBase10;
+using Mdt::UicNumber::tenThousandsDigitFromValueBase10;
+using Mdt::UicNumber::hundredThousandsDigitFromValueBase10;
 using Mdt::UicNumber::isDigit;
 using Mdt::UicNumber::isSpace;
 using Mdt::UicNumber::isAllowedChar;
@@ -55,6 +62,22 @@ TEST_CASE("int8FromChar")
   }
 }
 
+TEST_CASE("charFromInt8")
+{
+  SECTION("0")
+  {
+    REQUIRE( charFromInt8(0) == '0' );
+  }
+  SECTION("5")
+  {
+    REQUIRE( charFromInt8(5) == '5' );
+  }
+  SECTION("9")
+  {
+    REQUIRE( charFromInt8(9) == '9' );
+  }
+}
+
 TEST_CASE("toInt")
 {
   SECTION("00")
@@ -85,6 +108,309 @@ TEST_CASE("toInt")
   SECTION("94857560253 7560253")
   {
     REQUIRE( toInt("94857560253", 4, 7) == 7560253 );
+  }
+}
+
+TEST_CASE("onceDigitFromValueBase10")
+{
+  SECTION("0")
+  {
+    REQUIRE( onceDigitFromValueBase10(0) == 0 );
+  }
+
+  SECTION("1")
+  {
+    REQUIRE( onceDigitFromValueBase10(1) == 1 );
+  }
+
+  SECTION("9")
+  {
+    REQUIRE( onceDigitFromValueBase10(9) == 9 );
+  }
+
+  SECTION("10")
+  {
+    REQUIRE( onceDigitFromValueBase10(10) == 0 );
+  }
+
+  SECTION("11")
+  {
+    REQUIRE( onceDigitFromValueBase10(11) == 1 );
+  }
+
+  SECTION("12")
+  {
+    REQUIRE( onceDigitFromValueBase10(12) == 2 );
+  }
+
+  SECTION("19")
+  {
+    REQUIRE( onceDigitFromValueBase10(19) == 9 );
+  }
+
+  SECTION("123")
+  {
+    REQUIRE( onceDigitFromValueBase10(123) == 3 );
+  }
+
+  SECTION("1234")
+  {
+    REQUIRE( onceDigitFromValueBase10(1234) == 4 );
+  }
+}
+
+TEST_CASE("tensDigitFromValueBase10")
+{
+  SECTION("0")
+  {
+    REQUIRE( tensDigitFromValueBase10(0) == 0 );
+  }
+
+  SECTION("9")
+  {
+    REQUIRE( tensDigitFromValueBase10(9) == 0 );
+  }
+
+  SECTION("10")
+  {
+    REQUIRE( tensDigitFromValueBase10(10) == 1 );
+  }
+
+  SECTION("11")
+  {
+    REQUIRE( tensDigitFromValueBase10(11) == 1 );
+  }
+
+  SECTION("12")
+  {
+    REQUIRE( tensDigitFromValueBase10(12) == 1 );
+  }
+
+  SECTION("19")
+  {
+    REQUIRE( tensDigitFromValueBase10(19) == 1 );
+  }
+
+  SECTION("20")
+  {
+    REQUIRE( tensDigitFromValueBase10(20) == 2 );
+  }
+
+  SECTION("99")
+  {
+    REQUIRE( tensDigitFromValueBase10(99) == 9 );
+  }
+
+  SECTION("100")
+  {
+    REQUIRE( tensDigitFromValueBase10(100) == 0 );
+  }
+
+  SECTION("101")
+  {
+    REQUIRE( tensDigitFromValueBase10(101) == 0 );
+  }
+
+  SECTION("123")
+  {
+    REQUIRE( tensDigitFromValueBase10(123) == 2 );
+  }
+
+  SECTION("1234")
+  {
+    REQUIRE( tensDigitFromValueBase10(1234) == 3 );
+  }
+}
+
+TEST_CASE("hundredsDigitFromValueBase10")
+{
+  SECTION("0")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(0) == 0 );
+  }
+
+  SECTION("9")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(9) == 0 );
+  }
+
+  SECTION("99")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(99) == 0 );
+  }
+
+  SECTION("100")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(100) == 1 );
+  }
+
+  SECTION("101")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(101) == 1 );
+  }
+
+  SECTION("123")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(123) == 1 );
+  }
+
+  SECTION("246")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(246) == 2 );
+  }
+
+  SECTION("1234")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(1234) == 2 );
+  }
+
+  SECTION("2468")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(2468) == 4 );
+  }
+
+  SECTION("12345")
+  {
+    REQUIRE( hundredsDigitFromValueBase10(12345) == 3 );
+  }
+}
+
+TEST_CASE("thousandsDigitFromValueBase10")
+{
+  SECTION("0")
+  {
+    REQUIRE( thousandsDigitFromValueBase10(0) == 0 );
+  }
+
+  SECTION("999")
+  {
+    REQUIRE( thousandsDigitFromValueBase10(999) == 0 );
+  }
+
+  SECTION("1000")
+  {
+    REQUIRE( thousandsDigitFromValueBase10(1000) == 1 );
+  }
+
+  SECTION("1001")
+  {
+    REQUIRE( thousandsDigitFromValueBase10(1001) == 1 );
+  }
+
+  SECTION("123")
+  {
+    REQUIRE( thousandsDigitFromValueBase10(123) == 0 );
+  }
+
+  SECTION("1234")
+  {
+    REQUIRE( thousandsDigitFromValueBase10(1234) == 1 );
+  }
+
+  SECTION("12345")
+  {
+    REQUIRE( thousandsDigitFromValueBase10(12345) == 2 );
+  }
+}
+
+TEST_CASE("tenThousandsDigitFromValueBase10")
+{
+  SECTION("0")
+  {
+    REQUIRE( tenThousandsDigitFromValueBase10(0) == 0 );
+  }
+
+  SECTION("9999")
+  {
+    REQUIRE( tenThousandsDigitFromValueBase10(9999) == 0 );
+  }
+
+  SECTION("10'000")
+  {
+    REQUIRE( tenThousandsDigitFromValueBase10(10000) == 1 );
+  }
+
+  SECTION("10'001")
+  {
+    REQUIRE( tenThousandsDigitFromValueBase10(10001) == 1 );
+  }
+
+  SECTION("123")
+  {
+    REQUIRE( tenThousandsDigitFromValueBase10(123) == 0 );
+  }
+
+  SECTION("1'234")
+  {
+    REQUIRE( tenThousandsDigitFromValueBase10(1234) == 0 );
+  }
+
+  SECTION("12'345")
+  {
+    REQUIRE( tenThousandsDigitFromValueBase10(12345) == 1 );
+  }
+
+  SECTION("123'456")
+  {
+    REQUIRE( tenThousandsDigitFromValueBase10(123456) == 2 );
+  }
+}
+
+TEST_CASE("hundredThousandsDigitFromValueBase10")
+{
+  SECTION("0")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(0) == 0 );
+  }
+
+  SECTION("999")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(999) == 0 );
+  }
+
+  SECTION("99'999")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(99999) == 0 );
+  }
+
+  SECTION("100'000")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(100000) == 1 );
+  }
+
+  SECTION("100'001")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(100001) == 1 );
+  }
+
+  SECTION("100'002")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(100'002) == 1 );
+  }
+
+  SECTION("123")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(123) == 0 );
+  }
+
+  SECTION("1'234")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(1234) == 0 );
+  }
+
+  SECTION("12'345")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(12345) == 0 );
+  }
+
+  SECTION("123'456")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(123456) == 1 );
+  }
+
+  SECTION("1'234'567")
+  {
+    REQUIRE( hundredThousandsDigitFromValueBase10(1234567) == 2 );
   }
 }
 
